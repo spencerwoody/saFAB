8a9540b687c3afd6586c0d379739ba28ef8e7fa8
Spencer Woody <spencer.woody@utexas.edu>
2018-08-12 16:44:06 -0500

    Change DESCRIPTION to stop UTF warning

--- R/dtnorm.R
+++ R/dtnorm.R
@@ -1,19 +1,19 @@
 
-#' Density for the doulbe-truncated normal distribution, x ~ N(mean,
+#'  Density for the doulbe-truncated normal distribution, x ~ N(mean,
 #' sd^2) * 1(abs(x) > t)
 #'
-#' @param x vector of values at which to evaluate density
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param x vector of values at which to evaluate density 
 #' @param mean vector of means
 #' @param sd vector of standard deviations
-#' @param t truncation point
-#' @param log logical; if TRUE, probability given as log(p)
-#' @return The density of the double-truncated normal distribution at x
-#' @examples
-#' dtnorm(3, 0, 1, 1)
-#' dtnorm(0.5, 0, 1, 1)
+#' @param t truncation value
+#' @param log.p logical; if TRUE, probability given as log(p)
+#' @return The density of the double-truncated normal distribution at x 
+#' @author Spencer Woody
 #'
-#'
-
+#' @export
 dtnorm <- function(x, mean, sd, t, log.p = FALSE) {
   require(dplyr)
 
