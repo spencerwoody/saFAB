1b725241296bf23bd7deedf33fb50cb1977fea93
Spencer Woody <spencer.woody@utexas.edu>
2018-08-13 14:44:19 -0500

    Add empirical Bayes functions, and FAB functions

---
+++ R/FABfuns.R
@@ -0,0 +1,111 @@
+
+# FAB functions ---------------------------------------------------------
+
+# Derivative of Bayes risk (expected Lebesque measure) for spike-slab prior,
+# w.r.t. w, for calculating w(theta), the optimal spending functions
+Hprime_fab <- function(w, theta, p, sigma, tau, alpha) {
+  
+  p * sigma * alpha / sqrt(sigma^2 + tau^2) * 
+    exp(- {theta - sigma * qnorm(alpha * (1 - w))}^2 / (2 * sigma^2 + tau^2) + 
+    {qnorm(alpha * (1 - w))}^2 / 2) +
+    (1 - p) * alpha * 
+    exp(- {theta - sigma * qnorm(alpha * (1 - w))}^2 / (2 * sigma^2) + 
+    {qnorm(alpha * (1 - w))}^2 / 2) - 
+    p * sigma * alpha / sqrt(sigma^2 + tau^2) * 
+    exp(- {theta - sigma * qnorm(1 - alpha * w)}^2 / (2 * sigma^2 + tau^2) + 
+    {qnorm(1 - alpha * w)}^2 / 2) -
+    (1 - p) * alpha * 
+    exp(- {theta - sigma * qnorm(1 - alpha * w)}^2 / (2 * sigma^2) + 
+    {qnorm(1 - alpha * w)}^2 / 2)
+  
+}
+
+##' Make spending function for FAB setting, with spike-slab prior
+##'
+##' 
+##' @title make_w_fab
+##' @param sigma standard deviation of sampling distribution
+##' @param p prior probabilitiy of signal
+##' @param tau standard deviation of Gaussian slab
+##' @param alpha confidence level
+##' @param theta_min minimal value of theta to create spending function
+##' @param theta_max maximal value of theta to create spending function
+##' @param num_theta number of thetas over which to create grid 
+##' @param epsilon tolerance level
+##' @param verbose logical; if TRUE, print progress bar
+##' @return a vector of w's, corresponding to the spending function
+##' @author Spencer Woody
+##'
+##' @export
+make_w_fab <- function(sigma, p, tau, alpha = 0.1, theta_min = -7,
+                       theta_max = 7, num_theta = 5000,
+                       epsilon = 1e-5, verbose = F) {
+  require(dplyr)
+
+  theta_vec <- seq(theta_min, theta_max, length.out = num_theta)
+  
+  w_theta_fab <- rep(NA, num_theta)
+  
+  prog <- progress_estimated(num_theta)
+  
+  for (i in 1:length(theta_vec)) {
+    
+    w_theta_fab[i] <- uniroot(f = Hprime_fab, 
+                              interval = c(epsilon, 1 - epsilon), 
+                              theta = theta_vec[i],
+                              p = p,
+                              sigma = sigma, tau = tau, alpha = alpha)$root
+    if (verbose) {
+      prog$tick()$print()
+    }
+  }
+  
+  return(data.frame(theta = theta_vec, w))
+  
+}
+
+##' Lower bound of FAB interval
+##'
+##' 
+##' @title theta_l_fab
+##' @param theta_vec vector of thetas
+##' @param w_theta vector of w's for thetas (i.e., spending function)
+##' @param y single observation
+##' @param sigma standard deviation of sampling distribution
+##' @param alpha confidence level
+##' @return lower bound of FAB interval
+##' @author Spencer Woody
+##'
+##' @export
+theta_l_fab <- function(theta_vec, w_theta, y, sigma, alpha = 0.1) {
+  theta_l_fab <- y + sigma * qnorm(alpha * (1 - w_theta)) - theta_vec
+  
+  lower_quantile_fab <- y + 
+    sigma * qnorm(alpha * (1 - w_theta[which.min(abs(theta_l_fab))]))
+  
+  return(lower_quantile_fab)
+}
+
+##' Upper bound of FAB interval
+##'
+##' 
+##' @title theta_u_fab
+##' @param theta_vec vector of thetas
+##' @param w_theta vector of w's for thetas (i.e., spending function)
+##' @param y single observation
+##' @param sigma standard deviation of sampling distribution
+##' @param alpha confidence level
+##' @return upper bound of FAB interval
+##' @author Spencer Woody
+##'
+##' @export
+theta_u_fab <- function(theta_vec, w_theta, y, sigma, alpha = 0.1) {
+  
+  theta_u_fab <- y + sigma * qnorm(1 - alpha * w_theta) - theta_vec
+  
+  upper_quantile_fab <- y + 
+    sigma * qnorm(1 - alpha * w_theta[which.min(abs(theta_u_fab))])
+  
+  return(upper_quantile_fab)
+}
+
---
+++ R/ebayesFuns.R
@@ -0,0 +1,65 @@
+
+logit <- function(p) {
+  log(p / (1 - p))
+}
+
+inv_logit <- function(x) {
+  1 / (1 + exp(-x))
+}
+
+
+marginal_ss <- function(y, sigma, p, tau) {
+  ## Marginal of y using spike-slab-prior, but withOUT selection
+  
+  p * dnorm(y, 0, sqrt(sigma^2 + tau^2)) + 
+    (1 - p) * dnorm(y, 0, sigma)
+  
+}
+
+marginal_y_optim <- function(pars, y) {
+  ## Wrapper function for feeding into optim
+  
+  ## Inputs of par (which are in the set of reals)
+  ## pars[1] = log(sigma)
+  ## pars[2] = logit(p)
+  ## pars[3] = log(tau)
+
+  sigma <- exp(pars[1])
+  p <- inv_logit(pars[2])
+  tau <- exp(pars[3])
+  
+  ## Negative log likelihood
+  - sum(log(marginal_ss(y, sigma, p, tau)))
+  
+}
+
+##' Find empirical Bayes estimates of hyperparameters of spike-slab
+##' prior by using marginal likelihood maximization
+##'
+##' Theta follows the spike-slab prior
+##' @title eb_fun
+##' @param y vector of oberservation
+##' @return MMLE estimates of hyperparameters, the hessian matrix at
+##'   the MMLE estimate, and the convergence message
+##' @author Spencer Woody
+##'
+##' @export
+eb_fun <- function(y) {
+  
+  eb_optim <- optim(par = c(log(5), log(0.5), log(5)),
+                    fn = marginal_y_optim, 
+                    y = y,
+                    hessian = T)
+  
+  return(
+    list(
+      sigma_hat = eb_optim$par[1] %>% exp(),
+      p_hat = eb_optim$par[2] %>% inv_logit(),
+      tau_hat = eb_optim$par[3] %>% exp(),
+      H = eb_optim$hessian,
+      covergence = eb_optim$convergence
+    )
+  )
+  
+}
+
